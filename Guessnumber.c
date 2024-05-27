63b9056cd3317625b85ddc3d0889b0b4f69b359d
Puroo Kulshrestha <purookulshrestha012@gmail.com>
2024-05-27 11:31:33 +0530

    Update Guessnumber.c

--- Guessnumber.c
+++ Guessnumber.c
@@ -1,36 +1,57 @@
-#include <stdio.h>      
-#include <stdlib.h>     
-#include <time.h>       
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main ()
-{
-    int computer, my_num,i=0;
 
-    srand (time(NULL));
-    computer = rand() % 10 + 1;
+void clear_input_buffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
 
-    while(i<=4)
-   {
-  	if (i==4) {
-            printf("\nYou Loose!\n");
-            break;
+int main() {
+    int com, my_num, i;
+    char play_again;
+
+    do {
+        srand(time(NULL));
+        com = rand() % 10 + 1;
+        i = 0;
+
+        printf("Welcome to the Number Guessing Game!\n");
+        printf("I have chosen a number between 1 and 10. Can you guess it?\n");
+
+        while (i < 4) {
+            printf("\nAttempt %d of 4: Guess the number (1 to 10): ", i + 1);
+
+            
+            if (scanf("%d", &my_num) != 1 || my_num < 1 || my_num > 10) {
+                printf("Invalid input! Please enter a number between 1 and 10.\n");
+                clear_input_buffer(); // Clear the invalid input
+                continue;
+            }
+
+            if (com < my_num) {
+                printf("The secret number is lower.\n");
+            } else if (com > my_num) {
+                printf("The secret number is higher.\n");
+            } else {
+                printf("Congratulations: you guessed the number in %d attempts!\n", i + 1);
+                break;
+            }
+            i++;
         }
-        printf ("\nGuess the number (1 to 10): ");
-        scanf ("%d",&my_num);
-        if (computer<my_num && i<3) {
-           printf("The secret number is lower");
-           i++;
-	}
-	else if (computer>my_num && i<3){
-            printf("The secret number is higher");
-	    i++;
-	}
-	else
-	{
-            printf("Congratulations : you guessed the number in %d attempts!\n", ++i);
-    	    break;
-	}
-   }
 
-  return 0;
+        if (i == 4) {
+            printf("\nYou Lose! The secret number was %d.\n", com);
+        }
+
+        printf("\nDo you want to play again? (y/n): ");
+        clear_input_buffer(); // Clear any leftover input
+        play_again = getchar();
+
+    } while (play_again == 'y' || play_again == 'Y');
+
+    printf("Thank you for playing! Goodbye!\n");
+
+    return 0;
 }
